a57bfa25aa5e1458889f92d092ca6d6c402c88de
sajidalilogiclume <sajidali@logiclume.com>
2025-11-19 23:07:07 +0500

    message2

--- DSA/Pattern/DistortedSquare1.java
+++ DSA/Pattern/DistortedSquare1.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class TraingleVerticalFlipe {
-
+public class DistortedSquare1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print((char) (i + 64));
+            for (int j = 1; j <= n + 1 - i; j++) {
+                System.out.print("*");
             }
+
             System.out.println();
+        }
+
+    }
 }
